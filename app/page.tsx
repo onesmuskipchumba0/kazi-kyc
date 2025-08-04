0b66573bb10b6fbb6c21a45a0433c360a69b4361
onesmuskipchumba0 <onesmuskipchumba5@gmail.com>
2025-08-04 11:04:42 +0300

    update home

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
-    <div>
-      Hello world
+    <div className="w-full py-4">
+      <div className="relative flex flex-col w-full justify-center items-center">
+        <span className="text-2xl">Welcome to, <span className="font-semibold">Kazi KYC</span></span>
+        <div className="divider w-1/9 self-center divider-accent"></div>
+      </div>
     </div>
   );
 }
