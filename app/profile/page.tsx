af8f192565342466b569def489e45bd57a132f2c
onesmuskipchumba0 <onesmuskipchumba5@gmail.com>
2025-08-05 11:47:46 +0300

    add userinfo in profile

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,18 +1,27 @@
 'use client';
 import React from 'react'
 import { UserProfile, useClerk, useUser } from '@clerk/nextjs'
+
+
 function page() {
     const { loaded } = useClerk();
     const { user, isLoaded, isSignedIn } = useUser();
+
     !user || !isLoaded || !isSignedIn ? (
         <span className='loading loading-spinner loading-lg'></span>
     ) : (
         null
     )
   return (
-    <div className='relative flex flex-1  items-center justify-center w-full '>
+    <div className='relative flex flex-1 flex-col items-center justify-center w-full '>
         {loaded ? (
+            <>
+            <span className='text-2xl pb-3'>
+                Welcome back, 
+                <span className='font-semibold'> {user?.firstName}</span>
+            </span>
             <UserProfile routing='hash'/>
+            </>
         ): (
             <span className='loading loading-spinner loading-lg'></span>
         )}
--- lib/user/StateManagement.ts
+++ lib/user/StateManagement.ts
@@ -2,25 +2,30 @@
 import { create } from "zustand";
 import { useUser } from "@clerk/nextjs";
 
-const useUserStore = create((set) => {
-  userName : null
-  isLoading: false
-  error: null
+interface UserStore {
+  userName: string | null;
+  isLoading: boolean;
+  error: string | null;
+  setUsername: (user: { firstName: string } | null, isSignedIn: boolean) => void;
+}
 
-  // fetch user data
-  const setUsername = async () => {
-    set({isLoading: true, error: null})
-
-    try{
-      const { user, isSignedIn } = useUser();
-      user && isSignedIn ? 
-      set({ userName: user.fullName, isLoading: false, error: null}) :
-      set({error: "An error occured while loading user data"})
-    }
-    catch (err) {
-      set({ error: err });
+const { user, isSignedIn } = useUser(); 
+const useUserStore = create<UserStore>((set) => ({
+  userName: null,
+  isLoading: false,
+  error: null,
+  setUsername: async (user: { firstName: string } | null, isSignedIn: boolean) => {
+    set({ isLoading: true, error: null });
+    try {
+      if (user && isSignedIn) {
+        set({ userName: user.firstName, isLoading: false, error: null });
+      } else {
+        set({ error: "An error occured while loading user data", isLoading: false });
+      }
+    } catch (err: any) {
+      set({ error: err?.message || "Unknown error", isLoading: false });
     }
   }
+}));
 
-
-})
+export default useUserStore; 
