a81ef743935358acf01392d9b44cbd4efe718b4f
onesmuskipchumba0 <onesmuskipchumba5@gmail.com>
2025-09-10 09:07:48 +0300

    fix bugs for settings

--- components/profile/SettingsTab.tsx
+++ components/profile/SettingsTab.tsx
@@ -1,4 +1,3 @@
-"use client"
 import React from 'react';
 
 const SettingsTab: React.FC = () => {
@@ -13,28 +12,52 @@ const SettingsTab: React.FC = () => {
             <label className="label">
               <span className="label-text">Name</span>
             </label>
-            <input type="text" placeholder="Name" className="input input-bordered" value="John Mwangi" />
+            <input 
+              type="text" 
+              placeholder="Name" 
+              className="input input-bordered" 
+              value="John Mwangi"
+              readOnly
+            />
           </div>
           
           <div className="form-control">
             <label className="label">
               <span className="label-text">Email</span>
             </label>
-            <input type="email" placeholder="Email" className="input input-bordered" value="john.mwangi@email.com" />
+            <input 
+              type="email" 
+              placeholder="Email" 
+              className="input input-bordered" 
+              value="john.mwangi@email.com"
+              readOnly
+            />
           </div>
           
           <div className="form-control">
             <label className="label">
               <span className="label-text">Phone Number</span>
             </label>
-            <input type="tel" placeholder="Phone" className="input input-bordered" value="+254 712 345 678" />
+            <input 
+              type="tel" 
+              placeholder="Phone" 
+              className="input input-bordered" 
+              value="+254 712 345 678"
+              readOnly
+            />
           </div>
           
           <div className="form-control">
             <label className="label">
               <span className="label-text">Location</span>
             </label>
-            <input type="text" placeholder="Location" className="input input-bordered" value="Nairobi, Kenya" />
+            <input 
+              type="text" 
+              placeholder="Location" 
+              className="input input-bordered" 
+              value="Nairobi, Kenya"
+              readOnly
+            />
           </div>
         </div>
       </div>
@@ -46,14 +69,20 @@ const SettingsTab: React.FC = () => {
             <label className="label">
               <span className="label-text">Hourly Rate</span>
             </label>
-            <input type="text" placeholder="Hourly Rate" className="input input-bordered" value="KSh 600/hr" />
+            <input 
+              type="text" 
+              placeholder="Hourly Rate" 
+              className="input input-bordered" 
+              value="KSh 600/hr"
+              readOnly
+            />
           </div>
           
           <div className="form-control">
             <label className="label">
               <span className="label-text">Availability</span>
             </label>
-            <select className="select select-bordered">
+            <select className="select select-bordered" disabled>
               <option>Available immediately</option>
               <option>Available in 1 week</option>
               <option>Available in 2 weeks</option>
@@ -64,11 +93,16 @@ const SettingsTab: React.FC = () => {
           <div className="form-control">
             <label className="label cursor-pointer">
               <span className="label-text">Receive job notifications</span>
-              <input type="checkbox" className="toggle toggle-primary" defaultChecked />
+              <input 
+                type="checkbox" 
+                className="toggle toggle-primary" 
+                defaultChecked 
+                disabled
+              />
             </label>
           </div>
           
-          <button className="btn btn-primary">Save Changes</button>
+          <button className="btn btn-primary" disabled>Save Changes</button>
         </div>
       </div>
     </div>
